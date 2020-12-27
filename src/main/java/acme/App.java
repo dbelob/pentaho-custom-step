9c03198ede0cd363f51f2c6ea20ca5d942eff6d1
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2020-12-27 18:50:28 +0300

    Calculate MD5 hash

--- src/main/java/acme/App.java
+++ src/main/java/acme/App.java
@@ -1,10 +1,21 @@
 package acme;
 
-/**
- * Hello world!
- */
+import javax.xml.bind.DatatypeConverter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static String calculateMd5Hash(String password) throws NoSuchAlgorithmException {
+        MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+        messageDigest.update(password.getBytes());
+        byte[] digest = messageDigest.digest();
+
+        return DatatypeConverter.printHexBinary(digest).toUpperCase();
+    }
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        String md5Hash = calculateMd5Hash("password");
+
+        System.out.printf("MD5: %s\n", md5Hash);
     }
 }
--- src/test/java/acme/AppTest.java
+++ src/test/java/acme/AppTest.java
@@ -1,18 +1,42 @@
 package acme;
 
-import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.Assert.assertTrue;
+import java.security.NoSuchAlgorithmException;
+import java.util.stream.Stream;
 
-/**
- * Unit test for simple App.
- */
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+@DisplayName("App class tests")
 public class AppTest {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
     public void shouldAnswerWithTrue() {
         assertTrue(true);
+    }
+
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    @DisplayName("calculateMd5Hash method tests")
+    class CalculateMd5HashTest {
+        private Stream<Arguments> data() {
+            return Stream.of(
+                    arguments("", "D41D8CD98F00B204E9800998ECF8427E"),
+                    arguments("password", "5F4DCC3B5AA765D61D8327DEB882CF99")
+            );
+        }
+
+        @ParameterizedTest
+        @MethodSource("data")
+        void calculateMd5Hash(String password, String expected) throws NoSuchAlgorithmException {
+            assertEquals(expected, App.calculateMd5Hash(password));
+        }
     }
 }
